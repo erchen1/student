c22917563e4a75d554742fbcb96984a69e1f20a8
erchen1 <15635539803@163.com>
2018-12-15 15:12:24 +0800

    封装

--- src/main/java/com/tt/student/pojo/Student.java
+++ src/main/java/com/tt/student/pojo/Student.java
@@ -2,4 +2,12 @@ package com.tt.student.pojo;
 
 public class Student {
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
